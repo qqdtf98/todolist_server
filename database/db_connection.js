20678e02bbd274698231d41901c4bf4537a06ae9
qqdtf98 <qqdtf98@gmail.com>
2020-05-06 16:32:01 +0900

    feat: use mysql for delete function

--- database/db_connection.js
+++ database/db_connection.js
@@ -15,6 +15,20 @@ const todoData = {
     })
   },
 
+  deleteList: async function (query) {
+    return new Promise((resolve) => {
+      let sql = `delete from ${query.type} where id = ${query.index}`
+      connection.query(sql)
+      sql = `select * from ${query.type}`
+      connection.query(sql, function (err, results, field) {
+        const list = JSON.parse(JSON.stringify(results))
+        for (let i = 0; i < list.length; i++) {
+          list[i].date = list[i].date.split('T')[0]
+        }
+        resolve(list)
+      })
+    })
+  },
 }
 
 module.exports = todoData
---
+++ server/server_request_deleteList.js
@@ -0,0 +1,36 @@
+const todoData = require('../database/db_connection')
+
+function handleDelete(response, parsedQuery) {
+  if (parsedQuery.listType == 'todo') {
+    const queryData = {
+      type: 'todo_list',
+      index: parsedQuery.index,
+    }
+    todoData.deleteList(queryData).then((res) => {
+      const todoList = res
+      console.log(todoList)
+      response.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      })
+      response.end(JSON.stringify(todoList))
+    })
+  } else if (parsedQuery.listType == 'done') {
+    const queryData = {
+      type: 'done_list',
+      index: parsedQuery.index,
+    }
+    todoData.deleteList(queryData).then((res) => {
+      const doneList = res
+      response.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Access-Control-Allow-Origin': '*',
+      })
+      response.end(JSON.stringify(doneList))
+    })
+  }
+}
+
+module.exports = {
+  handleDelete,
+}
